c749de5e1995cd203abc28134db8ddf712324411
Son0h2 <son4216@ajou.ac.kr>
2024-11-21 12:59:55 +0900

    11/21 frontend memoryplansleect adding and searchmemory noresult image adding

--- frontend/src/components/Footernav.js
+++ frontend/src/components/Footernav.js
@@ -9,10 +9,11 @@ const FooterNav = () => {
     <div className="footer-nav">
       <button onClick={() => navigate('/home')}>홈</button>
       <button onClick={() => navigate('/record')}>스토리<br></br>기록하기</button>
-      <button onClick={() => navigate('/memory-plan')}>추억<br></br>계획하기</button>
+      <button onClick={() => navigate('/memory-plan/select')}>추억<br></br>계획하기</button>
       <button onClick={() => navigate('/mypage')}>마이<br></br>페이지</button>
     </div>
   );
+  
 };
-
+<FooterNav />
 export default FooterNav;
